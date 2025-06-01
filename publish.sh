4338a4e469a5648fd5714bfb1366cbf679097785
Anand Saha <anandsaha@gmail.com>
2025-05-31 22:20:08 -0700

    Updates

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting to generate site ..."
+
+cd teleported.in_hugo
+hugo
+cd ..
+cp -r teleported.in_hugo/public .
+git add .
+git commit -m "Updates"
+git push
+
+echo "Done"
