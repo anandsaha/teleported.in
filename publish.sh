9665842d0a0e7917d8a4ca0f432feb72191a833f
Anand Saha <anandsaha@gmail.com>
2025-09-04 22:26:54 -0700

    Updates

--- publish.sh
+++ publish.sh
@@ -7,9 +7,9 @@ echo "Starting to generate site ..."
 cd teleported.in_hugo
 hugo
 cd ..
-cp -r teleported.in_hugo/public .
+cp -r teleported.in_hugo/public/* .
 git add .
 git commit -m "Updates"
-# git push
+git push
 
 echo "Done"
